575642b7e68e4a4fdd68c5773942bdcddb5b6005
fifthaccess <benedikt.erkner-sach@bulme.at>
2021-09-21 12:52:05 +0200

    kleine änderrungen

---
+++ blechrechner/ConsoleApplication1/ConsoleApplication1.c
@@ -0,0 +1,28 @@
+// ConsoleApplication1.cpp : Diese Datei enthält die Funktion "main". Hier beginnt und endet die Ausführung des Programms.
+//
+
+#include <stdio.h>
+
+int main()
+{
+	/*char eingabe ;
+    //std::cout << "Hello World!\n";
+	//printf("Hello world");
+	gets_s(eingabe, 1);
+	printf(eingabe);
+	*/
+	char line[21]; 
+	gets_s(line, 20);
+	printf("The line entered was: %s\n", line);
+}
+
+// Programm ausführen: STRG+F5 oder "Debuggen" > Menü "Ohne Debuggen starten"
+// Programm debuggen: F5 oder "Debuggen" > Menü "Debuggen starten"
+
+// Tipps für den Einstieg: 
+//   1. Verwenden Sie das Projektmappen-Explorer-Fenster zum Hinzufügen/Verwalten von Dateien.
+//   2. Verwenden Sie das Team Explorer-Fenster zum Herstellen einer Verbindung mit der Quellcodeverwaltung.
+//   3. Verwenden Sie das Ausgabefenster, um die Buildausgabe und andere Nachrichten anzuzeigen.
+//   4. Verwenden Sie das Fenster "Fehlerliste", um Fehler anzuzeigen.
+//   5. Wechseln Sie zu "Projekt" > "Neues Element hinzufügen", um neue Codedateien zu erstellen, bzw. zu "Projekt" > "Vorhandenes Element hinzufügen", um dem Projekt vorhandene Codedateien hinzuzufügen.
+//   6. Um dieses Projekt später erneut zu öffnen, wechseln Sie zu "Datei" > "Öffnen" > "Projekt", und wählen Sie die SLN-Datei aus.
